b78df93c83c118f02b9f579461d30eb706b84db6
Mohammad Jumah <mohammad_jumah@outlook.com>
2023-05-03 16:19:33 +0100

    adding response to month

--- agriculture.py
+++ agriculture.py
@@ -2,18 +2,22 @@
 print('What do you want to grow?')
 wish = input()
 
+
 thisdict = {
   "Wheat": "January",
   "Chickpeas": "February",
   "Cumin": "March"
 }
 
+month = thisdict[wish]
+
 if wish in thisdict:
     print(f"Yes, {wish} is one a product I can grow for you")
+    print(f"You have to grow {wish} in {month}")
 
 else:
-    print (f"I am afraid, {wish} is a product I cannot grow for you")
+    print (f"I am afraid, {wish} is a product I cannot grow for you") 
 
 print('This is what I can grow for you')
-x = thisdict.keys()
-print (x)
+capability = thisdict.keys()
+print (capability)
